c6b896b05c203b5c041b5f4f4cc6e8474f44d3f1
Sebastian Oliva <code@sebastianoliva.com>
2016-10-27 02:52:54 -0600

    adding crud for patients

---
+++ nutriapp/anthrocalc/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from .views import (
+    PatientList,
+    PatientDetail,
+    PatientCreation,
+    PatientUpdate,
+    PatientDelete
+)
+
+urlpatterns = [
+    url(r'^patient/$', PatientList.as_view(), name='list'),
+    url(r'^patient/(?P<pk>\d+)$', PatientDetail.as_view(), name='detail'),
+    url(r'^patient/new$', PatientCreation.as_view(), name='new'),
+    url(r'^patient/edit/(?P<pk>\d+)$', PatientUpdate.as_view(), name='edit'),
+    url(r'^patient/delete/(?P<pk>\d+)$', PatientDelete.as_view(), name='delete'),
+]
--- nutriapp/anthrocalc/views.py
+++ nutriapp/anthrocalc/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import *
 
-# Create your views here.
+
+class PatientList(ListView):
+    model = Patient
+
+
+class PatientDetail(DetailView):
+    model = Patient
+
+
+class PatientCreation(CreateView):
+    model = Patient
+    success_url = reverse_lazy('patients:list')
+    fields = ['name', 'dob', 'family']
+
+
+class PatientUpdate(UpdateView):
+    model = Patient
+    success_url = reverse_lazy('patients:list')
+    fields = ['name', 'dob', 'family']
+
+
+class PatientDelete(DeleteView):
+    model = Patient
+    success_url = reverse_lazy('patients:list')
