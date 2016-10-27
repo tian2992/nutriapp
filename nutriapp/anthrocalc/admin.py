70dd85fb83bfed899b32f89be97fc019dfa2d655
Sebastian Oliva <code@sebastianoliva.com>
2016-10-27 00:51:25 -0600

    registering for admin crud

--- nutriapp/anthrocalc/admin.py
+++ nutriapp/anthrocalc/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(Visit)
+admin.site.register(Metric)
+admin.site.register(Action)
