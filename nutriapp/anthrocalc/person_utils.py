77624d95532f99582681de494faab51bec328153
Sebastian Oliva <code@sebastianoliva.com>
2018-02-26 14:50:47 -0600

    person utils returns lists

--- nutriapp/anthrocalc/person_utils.py
+++ nutriapp/anthrocalc/person_utils.py
@@ -4,7 +4,13 @@ def fetch_historical_metrics(person_id):
     visits = Visit.objects.filter(patient=person_id)
     metrics = {}
     for v in visits:
-        mets = Metric.objects.filter(visit=v)
-        metrics[v.date] = list(mets)
+        try:
+            mets = Metric.objects.filter(visit=v)[0]
+        except:
+            mets = None
+        metrics[v.id] = {"date": v.date, "metrics": mets}
 
     return metrics
+
+def calculate_indexes():
+    pass
