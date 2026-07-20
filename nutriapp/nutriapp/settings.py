e4cfd6fd29ccec68b41883b71fb347fee1f88359
tian <code@sebastianoliva.com>
2026-07-20 12:59:10 -0600

    Docker container

--- nutriapp/nutriapp/settings.py
+++ nutriapp/nutriapp/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
