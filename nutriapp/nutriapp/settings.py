6e71dd382c86e8c896a18811c823553b95655a7d
tian <code@sebastianoliva.com>
2026-08-16 21:28:27 -0600

    docker dotenv y djurl

--- nutriapp/nutriapp/settings.py
+++ nutriapp/nutriapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables from .env
@@ -94,10 +95,11 @@ WSGI_APPLICATION = "nutriapp.wsgi.application"
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
